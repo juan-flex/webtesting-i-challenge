ece92128e18908e35d1a671f93241c81c226508d
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-07-23 15:04:47 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,20 +1,26 @@
 module.exports = {
-  success,
+  succeed,
   fail,
   repair,
   get,
 };
 
-function success(item) {
-  return { ...item };
+function succeed(item) {
+  return { ...item, 
+    enhancement: item.enhancement === 20 ? item.enhancement : item.enhancement + 1 };
 }
 
 function fail(item) {
-  return { ...item };
+  return { ...item,
+    durability: item.durability - (item.enhancement >= 15 ? 10 : 5),
+    enhancement: item.enhancement - (item.enhancement > 16 ? 1 : 0)
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, 
+    durability: 100 
+  };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,53 @@
-const enhancer = require('./enhancer.js');
-// test away!
+const { succeed, fail, repair } = require('./enhancer.js');
+
+// item we will put through each test
+const item = {
+    name: 'item',
+    durability: 60,
+    enhancement: 17,
+}
+
+// expected results
+const itemSuccess = {
+    name: 'item',
+    durability: 60,
+    enhancement: 18,
+}
+const itemFail = {
+    name: 'item',
+    durability: 50,
+    enhancement: 16
+}
+const itemRepair = {
+    name: 'item',
+    durability: 100,
+    enhancement: 17
+}
+
+
+
+//tests!
+describe('enhancerTests', () => {
+    // success
+    describe('success', () => {
+        it('should accept item object and return new item based on rules', () => {
+            const newItem = succeed(item);
+            expect(newItem).toStrictEqual(itemSuccess);
+        })
+    })
+    // fail
+    describe('fail', () => {
+        it('should accept item object and return new item based on rules', () => {
+            const newItem = fail(item);
+            expect(newItem).toStrictEqual(itemFail);
+        })
+    })
+    // repair
+    describe('repair', () => {
+        it('should accept item object and return new item based on rules', () => {
+            const newItem = repair(item);
+            expect(newItem).toStrictEqual(itemRepair);
+        })
+    })
+
+})
